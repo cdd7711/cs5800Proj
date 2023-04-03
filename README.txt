03e5762329f4f1fae16995eae82870ae97ca30ce
cdd7711 <cassandra.d.dolsen@wmich.edu>
2023-04-03 13:20:19 -0400

    Filled in some functions. runPDA and the class methods are what's left

---
+++ README.txt
@@ -0,0 +1,12 @@
+pg224.txt is a sample PDA from the textbook.
+
+Read these PDAs as follows:
+1st line is possible states separated by comma without spaces
+2nd line is the possible char/terminals separated by comma without spaces
+3rd line is possible stack char separated by comma without spaces
+4th line is all the accepting states separated by comma without spaces
+5th line is the start state
+
+All following lines in the PDA files are transitions. each line is one transition.
+start state,terminal,stack head to pop,new state,new stack head pushed
+comma separated without spaces
--- main.py
+++ main.py
@@ -10,10 +10,11 @@ class PDA:
         self.states = states
         self.currentState = self.startState
 
-    # todo: finish/organize methods
+    # todo: finish/organize methods. Delete any not needed
 
     # getStackLang might not be needed
-    def getStackLang(self):  # maybe make this a function that takes a char and returns if it is an element of the stack string?
+    def getStackLang(self):
+    # maybe make this a function that takes a char and returns if it is an element of the stack string?
         return self.stackLang
 
     def getStackHead(self):
@@ -33,14 +34,15 @@ class PDA:
     def setState(self, state):
         self.currentState = state
 
+
 def main():
-    # gets, validates, and opens PDA file
+    # gets, validates, and opens PDA file. same pda file for each string
     while True:
         try:
             pdaFile = open(input("What PDA text file do you want to open?\n"), "r")
             break
         except FileNotFoundError:
-            print("That is not a valid file name.")
+            print('That is not a valid file name. An example file is "pg224.txt"')
 
     # gets, validates, and opens string file
     while True:
@@ -48,11 +50,16 @@ def main():
             stringFile = open(input("What string text file do you want to open?\n"), "r")
             break
         except FileNotFoundError:
-            print("That is not a valid file name.")
+            print('That is not a valid file name. An example file is "strings.txt"')
 
-    pdaClass = fillPDA(pdaFile)
-
-    # todo: for each line in string file, send PDA and string to runPDA
+    # for each line in string file, send PDA and string to runPDA
+    for line in stringFile:
+        pdaClass = fillPDA(pdaFile)
+        result = runPDA(pdaClass, line.strip("\n"))
+        if result is True:
+            print("Your PDA with string", line.strip("\n"), "was accepted.")
+        else:
+            print("Your PDA with string", line.strip("\n"), "was NOT accepted.")
 
     # close files
     pdaFile.close()
@@ -60,24 +67,35 @@ def main():
 
 
 def runPDA(pda, string):
-    # todo: fill this in recursively to run through the given PDA with a string
-    # todo: print if accepted/not accepted
+    # todo: fill this in recursively to run through the given PDA (class) with a string
+    # todo: return True/False bool value if accepted true
+    # todo: Possibly test that each terminal and stack char are elements of their languages
 
 
 def fillPDA(pdaFile):
     # organize a text file to have start state, accepting states, transitions, stack language, terminal language
 
-    states = pdaFile.readline().split(",")  # list
-    terminalLang = pdaFile.readline().split(",")  # list
-    stackLang = pdaFile.readline().split(",")  # list
-    accepting = pdaFile.readline().split(",")  # list
+    states = pdaFile.readline().strip("\n").split(",")  # list
+    terminalLang = pdaFile.readline().strip("\n").split(",")  # list
+    stackLang = pdaFile.readline().strip("\n").split(",")  # list
+    accepting = pdaFile.readline().strip("\n").split(",")  # list
     start = pdaFile.readline().strip("\n")  # list
 
-    # todo: rest of file is transitions. Read these into dictionary transitions
+    transitions = {}  # dict with tuple: tuple
+    # key is (start state, terminal, stack head to pop)
+    # value is (new state, new stack head pushed)
 
-    pdaClass = PDA(stackLang, terminalLang, transitions, start, accepting, states)
+    line = pdaFile.readline()
+    while line:
+        line = line.strip("\n").split(",")
+        key = (line[0], line[1], line[2])
+        value = (line[3], line[4])
+        transitions[key] = value
 
-    return pdaClass
+        line = pdaFile.readline()
+
+    return PDA(stackLang, terminalLang, transitions, start, accepting, states)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
